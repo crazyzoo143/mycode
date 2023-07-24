14a4c1ff05254c43fd9dd55aad99cfcd2a64c872
crazyzoo143 <devcrazyzoo143@gmail.com>
2023-07-24 15:33:22 +0000

    finalfantasy2.py

--- finalfantasyapi/finalfantasy2.py
+++ finalfantasyapi/finalfantasy2.py
@@ -1,14 +1,18 @@
 import requests
 
 def search_character_by_name(name):
-    base_url = "https://www.moogleapi.com/api/v1/characters/search"
-    params = {"name": name}
+    # example URL: https://www.moogleapi.com/api/v1/characters/search?name=cloud
+    base_url = "https://www.moogleapi.com/api/v1/characters/search?name=" + name
+    print(base_url)
+    return base_url
 
 if __name__ == "__main__":
     character_name = input("Enter the character name to search for: ")
-    character_data = search_character_by_name(character_name)
+    character_url = search_character_by_name(character_name)
 
-    if character_data:
+    character_data= requests.get(character_url).json()
+
+    if isinstance(character_data, list):
         print("Character found!")
         print("Character Data:")
         print(character_data)
