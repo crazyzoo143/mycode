c16e00adfedd61577e0937a2549fc0a4d6134d23
Maria <maria@example.com>
2023-08-09 14:56:51 +0000

    saving my work

--- staticweblab/staticwebemails.py
+++ staticweblab/staticwebemails.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 email_addresses = [
     "user1@fakemeial.com",
     "user2@fakeemail.com",
-    "user3@fakeemail.com"
-    "user4@fakeemail.com"
+    "user3@fakeemail.com",
+    "user4@fakeemail.com",
     "user5@fakeemail.com"
 ]
 
@@ -15,5 +15,5 @@ def index():
     return render_template('index.html', email_addresses=email_addresses)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=2224)
 
